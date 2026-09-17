da948f4e178dcea0ef33bb4a50dcd123d90d6846
philip-voultsos <philip@voultsos.com>
2026-09-17 14:29:17 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
     site: "https://example.com",
-    output: "hybrid",
+    output: "static",
     integrations: [mdx(), sitemap(), react(), keystatic()],
     adapter: cloudflare({
         platformProxy: {
